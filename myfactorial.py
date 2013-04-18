93200a6800d13cc0e3aa87acca50a6a433b0accb
skim9142 <seankim9142@gmail.com>
2013-04-17 17:41:48 -0700

    Update myfactorial.py

--- myfactorial.py
+++ myfactorial.py
@@ -1,6 +1,10 @@
 def myfactorial(x):
     n = 1
-    while (x > 0):
-        n = n * x
-        x = x - 1
-    return n
+    if x%1 == 0:
+        while (x > 0):
+            n = n * x
+            x = x - 1
+        return n
+        print n
+    else:
+        print "Input must be an integer"
