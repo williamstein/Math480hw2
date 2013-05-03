dd7253bf11def1bacb079dedcc7f16216441d761
William Stein <wstein@gmail.com>
2013-05-03 10:25:15 -0700

    Update myfactorial.py

--- myfactorial.py
+++ myfactorial.py
@@ -1,10 +1,10 @@
 def myfactorial(x):
     n = 1
-    if x%1 == 0:
+    if x%1 == 0:        # bad way to test for int; use isinstance(x, (int, long))
         while (x > 0):
             n = n * x
             x = x - 1
         return n
-        print n
+        print n    # printing right after returning will never happen
     else:
         print "Input must be an integer"
